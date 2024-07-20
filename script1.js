15e9f3d9c9b6e55e2809f9c3bec5d24d5d2be36d
panwarkunal <monupanwar5655@gmail.com>
2024-07-20 19:53:33 +0530

    day 1 and 2

---
+++ script1.js
@@ -0,0 +1,73 @@
+// ✅ day 1 of 30 days coding challange;
+// 👩‍💻Todos=>revise all the basic cocept and all the
+
+// variable name only start with the letter
+// var value1 =10;
+// console.log(value1)
+
+// var first_name ="kunal";
+// var last_name ="panwar";
+// console.log(first_name+" "+last_name)
+
+// Convention 1. Use camelCase writing
+// var firstName = "kunalPanwar";
+// var lastName ="panwar";
+
+// console.log(firstName+" "+lastName)
+
+// Convention 2. Use snake_case writing
+//  var first_name ="kunal_panwar";
+//  console.log(first_name);
+
+// let
+// let value =10;
+// console.log(value);
+// value=30;
+// console.log(value)
+
+// const
+// const value =30;
+// console.log(value);
+
+// value =35;
+// console.log(value);
+
+// let x =30;
+// console.log(typeof x);
+
+// var abc;
+// console.log(typeof abc);
+
+// objects
+// let obj = {};
+
+// let person = {
+//   firstName: "kunal",
+//   lastName: "panwar",
+// };
+
+
+// const{firstName,lastName}=person;
+// console.log(firstName);
+// console.log(lastName);
+
+
+// data conversition
+
+// let first_name="kunal";
+// console.log(typeof first_name,"value",first_name);
+
+// let num =Number(first_name);
+// console.log(typeof num,"value",num);
+
+// let str1="1234";;
+// console.log(typeof str1,"value",str1);
+
+// let num =Number(str1);
+// console.log(typeof num,"value",num);
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,38 @@
+// ✅ day 1 of 30 days coding challange;
+// 👩‍💻Todos=>revise all the operators
+
+
+// let a=10;
+// let b=5;
+// console.log(a+b);
+
+
+// let a =20;
+// let b =20;
+// let sub =b-a;
+// let mul =a*b;
+// let mod =a%b;
+// let quotient =a/b;
+
+// console.log(sub);
+// console.log(quotient);
+// console.log( mod);
+// console.log(mul);
+
+// comparision operator
+// let a =10;
+// let b =10;
+// console.log(a===b);
+
+// let a =true;
+// let b =true;
+
+// console.log(a&&b);
+
+// ternary operation
+// let num =10;
+
+// let result =num>0?"positive":"false";
+// console.log(result);
+
+
