6c4c014e54c1d90ec2e1dd615e7779a4fd43dce6
panwarkunal <monupanwar5655@gmail.com>
2024-07-21 15:10:27 +0530

    day 4 loops

---
+++ script4.js
@@ -0,0 +1,38 @@
+// ✅ Day 4 loops 
+
+// write a program for printing number from 1 to 10
+// for(let i =0;i<=10;i++){
+//     console.log(i);
+// }
+
+// multiplication of 5 help of the for loop
+// const number =5;
+// for(let i =0;i<=10;i++){
+//     console.log(`${number} * ${i} = ${number*i}`);
+// }
+
+// while loop 
+// let sum = 0;
+// let i =1;
+// while(i<=10){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum);
+
+// reverse loop
+// let i =10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// do while loop
+// let i =1;
+// do{
+//     console.log(i);
+//     i++
+// }
+// while(i<=5);
+
+
