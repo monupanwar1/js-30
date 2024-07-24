37171b5416df13c6062953471717abf16a7ebce2
panwarkunal <monupanwar5655@gmail.com>
2024-07-24 19:33:22 +0530

    array complete

---
+++ script6.js
@@ -0,0 +1,54 @@
+// ✅Day 6 Array
+
+// let Array=[1,2,3,4,5];
+// console.log(Array);
+
+// how to acesss the Array;
+// let Array=[1,2,3,4,5];
+// console.log(Array[2]);
+
+
+
+// Array mehtod 
+// let Array=[1,2,3,4,5];
+// Array.push(10);
+// console.log(Array)
+// Array.pop(10);
+// console.log(Array)
+// Array.shift(10);
+// console.log(Array)
+// Array.unshift(10);
+// console.log(Array)
+
+// map implimantation
+
+// let Array=[1,2,3,4,5];
+// let doubleArray=Array.map((num)=>num*2);
+// console.log(doubleArray);
+
+
+// filter implimantation
+
+// let Array=[1,2,3,4,5];
+// let filter=Array.filter((num)=>num%2==0);
+// console.log(filter);
+
+
+// let array =[1,2,3,4,5];
+// let sum =array.reduce((acc,current)=>acc+current);
+// console.log(sum)
+
+// iteration on the array using the for each
+// let array =[1,2,3,4,5];
+// array.forEach((e)=>console.log(e));
+
+// iteration on the array by using the loop;
+
+// let array =[1,2,3,4,5];
+// for(let i=0;i<array.length;i++){
+//     console.log(i);
+// }
+
+
+
+
