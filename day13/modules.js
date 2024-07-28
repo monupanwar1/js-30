d59b47da21f48cfee9048876d9ff48ab82241d4f
panwarkunal <monupanwar5655@gmail.com>
2024-07-28 09:10:39 +0530

    day 13 modules

---
+++ day13/modules.js
@@ -0,0 +1,49 @@
+// // modules in js
+
+// function add1(a,b){
+//     return (a+b)
+//   }
+//   export {add1};
+  
+// export const person ={
+//     name:"kunal",
+//     age:25,
+//     greet(){
+//         return`hello my name is ${this.name} and i m ${this.age}`
+//     }
+// }  
+
+// export function add(a,b){
+//     return a+b;
+// }
+// export function substract(a,b){
+//     return a-b;
+// }
+
+
+
+// const mul =(a,b)=>{
+//     return a*b;
+
+
+// }
+// export default mul;
+
+
+// import axios from "axios";
+// axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
+// .then((res)=>console.log(res))
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
+export const PI =3.14;
+export const E =2.71;
+
+export function square(x){
+    return x*x;
+    
+}
+
+
---
+++ day13/script.js
@@ -0,0 +1,34 @@
+// Creating and Exporting Modules
+
+// import { add } from "./modules.js";
+
+// const result =add(3,5);
+// console.log(result);
+
+
+// Create a module that exports an object representing a person with
+//  properties and methods
+
+// import { person } from "./modules.js";
+// console.log(person.greet());
+
+
+//  Named and Default Exports
+// import {add,substract} from "./modules.js"
+
+
+// console.log(add(5,3))
+// console.log(substract(5,3))
+
+
+// import mul from "./modules.js"
+
+// console.log(mul(3,5));
+
+
+// Importing Entire Modules
+// import * as modules from "./modules.js"
+// console.log(modules.PI)
+// console.log(modules.E)
+// console.log(modules.square(2));
+
